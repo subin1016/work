16302914241829f6b6fcc466cca17517b7dd123d
subin1016 <qwe46200@naver.com>
2023-01-06 14:31:39 +0900

    workProject

---
+++ src/sample01/MainClass.java
@@ -0,0 +1,34 @@
+package sample01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("헬로우, 자바");
+		
+		// 한줄 주석
+		
+		/* 
+			범위 주석   
+		 */
+		
+		System.out.println("Hello World");
+		
+		System.out.print("Hello");
+		
+		System.out.print("월드");
+		
+		// 기능 : escape sequence 
+		// \n (개행), \b(backspace), \"(따움표를 출력), \t(tab), \\(\를 출력)
+		
+		System.out.print("World\n");
+		
+		System.out.println("헬로우\t헬로");
+		
+		System.out.printf("%s \n", "Hello");
+		
+		System.out.println(1024);
+
+	}
+
+}
